c14f2f734405c75459c29ef7d2d42ea410332a8f
Ivan Boyko <boyko_ivan@mail.ru>
2025-01-15 18:23:54 +0300

    Добавлены cors и nocache

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.4
 require (
 	github.com/alicebob/miniredis/v2 v2.34.0
 	github.com/go-chi/chi/v5 v5.2.0
+	github.com/go-chi/cors v1.2.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/redis/go-redis/v9 v9.7.0
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -6,12 +6,20 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/imboyko/lock-service/internal/storage"
 )
 
 func NewRouter(s *storage.RedisStorage, l *slog.Logger, jwtSecret string) http.Handler {
 	r := chi.NewRouter()
-	r.Use(middleware.RequestID, middleware.Recoverer, middleware.CleanPath, ContextLogger(l))
+	r.Use(
+		middleware.RequestID,
+		middleware.Recoverer,
+		middleware.CleanPath,
+		cors.AllowAll().Handler,
+		middleware.NoCache,
+		ContextLogger(l),
+	)
 
 	r.Route("/locks", func(r chi.Router) {
 		r.Get("/", HandleGetAll(s))
